50f4cf5659aea4fe478ff7ffbec6f8fd85815152
danriley11 <dan_riley7@outlook.com>
2024-01-24 17:59:49 +1100

    Build TOU style component

---
+++ src/pages/termsOfUse/TermsOfUse.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { rem } from '../../components/core/styles';
+
+const TermsOfUseContent = styled.div`
+  p {
+    margin: 0 ${rem(16)};
+  }
+`;
+
+export default TermsOfUseContent;
