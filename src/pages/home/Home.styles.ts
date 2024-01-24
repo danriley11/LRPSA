d9db5889476a692420a7998bd0b5e50054cdf72c
danriley11 <dan_riley7@outlook.com>
2024-01-24 16:40:12 +1100

    Build home styles

---
+++ src/pages/home/Home.styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { decorativePurple, textDarkPurple } from '../../components/core/colours';
+import { rem } from '../../components/core/styles';
+
+const HomeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    background-color: ${decorativePurple};
+    border-radius: ${rem(50)};
+    padding: 0 ${rem(48)};
+  }
+
+  h1,
+  h2 {
+    text-align: center;
+    color: white;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-weight: 100;
+
+    text-transform: uppercase;
+  }
+
+  p {
+    color: ${textDarkPurple};
+    text-align: center;
+  }
+
+  p:last-child {
+    text-align: center;
+    font-weight: bold;
+    font-style: italic;
+  }
+`;
+
+export default HomeContent;
